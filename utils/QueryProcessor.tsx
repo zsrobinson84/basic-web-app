b2c0027f0689f8dc56494d05db2fbe3065e62ed5
Zoe <zrobinso@andrew.cmu.edu>
2025-10-07 10:04:18 -0400

    committed name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "zrobinso";
   }
 
   return "";
